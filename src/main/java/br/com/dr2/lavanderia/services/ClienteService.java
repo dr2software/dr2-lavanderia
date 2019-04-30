bcc80e82636c86ddff929ee4285ae5674fca60d3
Diego Santos <diegosan.dev@gmail.com>
2019-04-30 00:31:04 -0300

    commit

--- src/main/java/br/com/dr2/lavanderia/services/ClienteService.java
+++ src/main/java/br/com/dr2/lavanderia/services/ClienteService.java
@@ -1,39 +1,7 @@
 package br.com.dr2.lavanderia.services;
 
-import java.util.List;
-
 import br.com.dr2.lavanderia.models.Cliente;
 
-public class ClienteService implements IService<Cliente> {
-
-	@Override
-	public Cliente inserir(Cliente entity) {
-		
-		return null;
-	}
-
-	@Override
-	public Cliente atualizar(Cliente entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Cliente buscarPorId(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deletarPorId(int id) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public List<Cliente> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public interface ClienteService extends IService<Cliente> {
 
 }
